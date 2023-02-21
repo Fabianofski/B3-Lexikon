0dd6c413bf701a23a6ccb4375918985372b646ea
fbnr1 <fr.1602.fr@gmail.com>
2023-02-21 02:38:11 +0100

    added token cookie & cookies for plants

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 8080;
+
+const sessions = [];
+
+app.use(cookieParser());
+
+app.use("/", (req, res, next) => {
+    console.log()
+    if(!req.cookies.token) {
+        const sessionId = Math.floor(Math.random() * 10000);
+
+        sessions.push(sessionId);
+
+        res.cookie("token", sessionId);
+    }   
+    next();
+})
+
+app.use("/html/:plant", (req, res, next) => {
+    const plant = req.params.plant.replace(".html", "");
+    let count = parseInt(req.cookies[plant]) || 0;
+    count++;
+    res.cookie(plant, count.toString());
+    next()
+});
 
 app.use("/", express.static("public"))
 
