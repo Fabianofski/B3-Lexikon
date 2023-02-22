f58297c36167547be4a5ed74b8a39c5687481a56
fbnr1 <fr.1602.fr@gmail.com>
2023-02-22 23:35:00 +0100

    added ejs and most viewed articles

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = 8080;
 
 const sessions = [];
 
+app.set('view engine', 'ejs');
+
 app.use(cookieParser());
 
 app.use("/", (req, res, next) => {
@@ -19,17 +21,63 @@ app.use("/", (req, res, next) => {
     next();
 })
 
+app.get("/index.html", (req, res, next) => {
+  res.redirect("/")
+})
+
+app.get("/", (req, res, next) => {
+  const articles = req.cookies
+
+  const filtered = Object.entries(articles)
+  .filter(([key]) => key.startsWith('views-'));
+
+  const mapped = filtered.map(([key, value]) => {
+    const newKey = key.slice('views-'.length);
+    return [newKey, value];
+  });
+
+  const sorted = mapped.sort(([, a], [, b]) => b - a);
+
+  const topThree = sorted.slice(0, 3);
+
+  const resultObj = topThree.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+
+  console.log(resultObj)
+  const firstKey = Object.keys(resultObj)[0]
+  const secondKey = Object.keys(resultObj)[1]
+  const thirdKey = Object.keys(resultObj)[2]
+  console.log(firstKey)
+  const top = { first: {
+    name: firstKey,
+    value: resultObj[firstKey],
+  }, second: {
+    name: secondKey,
+    value: resultObj[secondKey],
+  }, third: {
+    name: thirdKey,
+    value: resultObj[thirdKey],
+  }
+  }
+  res.render('pages/index', {
+    topThree: top
+  })
+})
+
 app.use("/plants/:plant", (req, res, next) => {
   const plant = req.params.plant
-
+  
   if (req.path !== "/" || plant.endsWith(".css")) {
     next()
     return
   }
   
-  let count = parseInt(req.cookies[plant]) || 0;
+  const cookieName = `views-${plant}`
+  let count = parseInt(req.cookies[cookieName]) || 0;
   count++;
-  res.cookie(plant, count.toString());
+  res.cookie(cookieName, count.toString());
   next()
 })
 
