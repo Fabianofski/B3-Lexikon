619122e21cd56ca81dd7c089f298cff26b039ef6
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-02-25 21:41:12 +0100

    Simplified Most viewed Logic

--- index.js
+++ index.js
@@ -6,76 +6,51 @@ const PORT = 8080;
 
 const sessions = [];
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  const articles = req.cookies
+  const cookies = req.cookies;
 
-  const filtered = Object.entries(articles)
-  .filter(([key]) => key.startsWith('views-'));
+  const articles = Object.entries(cookies).filter(([key]) => key.startsWith("views-"));
 
-  const mapped = filtered.map(([key, value]) => {
-    const newKey = key.slice('views-'.length);
+  const mapped = articles.map(([key, value]) => {
+    const newKey = key.slice("views-".length);
     return [newKey, value];
   });
-
   const sorted = mapped.sort(([, a], [, b]) => b - a);
-
-  const firstThree = sorted.slice(0, 3);
-
-  const resultObj = firstThree.reduce((acc, [key, value]) => {
-    acc[key] = value;
-    return acc;
-  }, {});
-
-  const firstKey = Object.keys(resultObj)[0]
-  const secondKey = Object.keys(resultObj)[1]
-  const thirdKey = Object.keys(resultObj)[2]
-
-  const topThree = { first: {
-    name: firstKey,
-    value: resultObj[firstKey],
-  }, second: {
-    name: secondKey,
-    value: resultObj[secondKey],
-  }, third: {
-    name: thirdKey,
-    value: resultObj[thirdKey],
-  }
-  }
-  res.render('pages/index', {
-    topThree: topThree
-  })
-})
+  res.render("pages/index", {
+    mostViewed: sorted
+  });
+});
 
 app.get("/plants/:plant/", (req, res, next) => {
-  const plant = req.params.plant
-  
+  const plant = req.params.plant;
+
   if (plant.endsWith(".css")) {
-    next()
-    return
+    next();
+    return;
   }
-  
-  const cookieName = `views-${plant}`
+
+  const cookieName = `views-${plant}`;
   let count = parseInt(req.cookies[cookieName]) || 0;
   count++;
   res.cookie(cookieName, count.toString());
-  next()
-})
+  next();
+});
 
 app.use((req, res, next) => {
-    if(!req.cookies["token"]) {
-        const sessionId = Math.floor(Math.random() * 10000);
+  if (!req.cookies["token"]) {
+    const sessionId = Math.floor(Math.random() * 10000);
 
-        sessions.push(sessionId);
+    sessions.push(sessionId);
 
-        res.cookie("token", sessionId);
-    }
-    next();
-})
+    res.cookie("token", sessionId);
+  }
+  next();
+});
 
-app.use("/", express.static("public"))
+app.use("/", express.static("public"));
 
-app.listen(PORT, () => console.log("Listening ..."))
+app.listen(PORT, () => console.log("Listening ..."));
